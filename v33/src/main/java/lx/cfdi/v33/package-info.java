8baac23cb92e6472045d861834c740b31a1e1a0e
Ruben Cancino <rubencancino6@gmail.com>
2017-05-08 12:28:27 -0500

    Initial commit

---
+++ v33/src/main/java/lx/cfdi/v33/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7-b41 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: PM.05.08 a las 12:08:00 PM CDT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.sat.gob.mx/cfd/3", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package lx.cfdi.v33;
